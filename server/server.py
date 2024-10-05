c08ca8efaf035540ce8ef80e35fe7d10cd32f5bd
Shah Anwar <shah.anwar@kcl.ac.uk>
2024-10-05 13:19:07 +0100

    Setting PORT Configs

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ import socket
 import asyncio
 import zmq
 import zmq.asyncio
+import os
 
 from .packet_handler import PacketHandler
 from .redis_client import RedisClient
@@ -11,7 +12,11 @@ class Server:
     def __init__(self):
         self.context = zmq.asyncio.Context()
         self.socket = self.context.socket(zmq.REP)
-        self.socket.bind("tcp://*:5555")
+
+        port = os.environ.get("PORT", "5555")
+        socket.bind(f"tcp://*:{port}")
+        print(f"Server listening on port {port}")
+
         self.packet_handler = PacketHandler()
 
     async def listen_for_packets(self):
